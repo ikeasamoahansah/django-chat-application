55835ac36dd9d220644dbd40efbee9b4b41f775f
Ike <dylanraspino264@gmail.com>
2023-08-28 02:02:38 +0000

    starting to create rooms

--- chatapp/chat/urls.py
+++ chatapp/chat/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.lobby, name='lobby'),
     path('create_room/', views.create_room, name="create_room"),
-    path('room/<str:pk>', views.room, name="room")
+    path('room/<str:pk>', views.room, name="room"),
+    path('signup', views.signup, name="signup"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout_view, name="logout")
 ]
--- chatapp/chat/views.py
+++ chatapp/chat/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, authenticate
+from django.contrib.auth import login as auth_login
+from .forms import *
 
 # Create your views here.
 
@@ -8,19 +11,22 @@ from django.contrib.auth.decorators import login_required
 def lobby(request):
     return render(request, 'chat/lobby.html')
 
-@login_required(login_url='/admin')
+@login_required(login_url='/login')
 def create_room(request):
-    if request.method == 'GET':
-        return render(request, 'chat/create_room.html')
-    elif request.method == 'POST':
-        name = request.POST['name']
-        
-        room = Room(name=name, creator=request.user)
-        room.save()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.creator = request.user
+            room.save()
+            return redirect('/')
+        else:
+            form = RoomForm()
+    return render(request, 'chat/create_room.html', {"form":form})
 
-        return redirect('')
 
-@login_required(login_url='/admin')
+
+@login_required(login_url='/login')
 def room(request, pk):
 
     room_get = Room.objects.get(id=pk)
@@ -30,3 +36,38 @@ def room(request, pk):
             "rooms": Room.objects.all()
         })
         
+
+def signup(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'chat/signup.html', {"form":form})
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'chat/login.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/home')
+        else:
+            return render(request, "registration/login.html", {
+                "msg": "Invalid login credentials"
+            })
+    else:
+        return HttpResponse('<h1>Failed attempt to login</h1>')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
